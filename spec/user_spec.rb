032034697f05dad382e8a233c6769cf315a4b941
Cory Spitzer & andrew david burt <Coryspitzer@gmail.com>
2015-05-12 16:44:31 -0700

    add answers to users via test.

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,9 +1,15 @@
 require('spec_helper')
 
-describe(Survey) do
+describe(User) do
   it("lists users") do
     user_1 = User.create(:name => "Bertha")
     expect(user_1.name()).to(eq("Bertha"))
+  end
+
+  it("will add a answer to a user") do
+    user_1 = User.create(:name => "Bartholomew")
+    answer_1 = Answer.create({:content => "I hate answering survey questions.", :user_id => user_1.id})
+    expect(user_1.answers()).to(eq([answer_1]))
   end
 
 end
